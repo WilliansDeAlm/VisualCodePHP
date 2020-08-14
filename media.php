8b0643564173dcc6d2c17197f8d0888c9fcc9212
Willians de Almeida <69019582+WilliansDeAlm@users.noreply.github.com>
2020-08-14 15:19:59 -0300

    Alteração via Web

--- media.php
+++ media.php
@@ -3,11 +3,11 @@
 $num = 0;
 
 for ($i = 1; $i<5 ;$i++) {
-	($numx [$i]= readline("digite o $i º número positivo inteiro: "));	
-	$num = $num + $numx[$i];
-	if ($i == 4){
-    $media = $num / $i;
-    $media = number_format($media, 2, '.', '');
-    echo  ("A Média dos números digitados é $media");
+    ($numx [$i]= readline("digite o $i º número positivo inteiro: "));	
+    $num = $num + $numx[$i];
+    if ($i == 4){
+        $media = $num / $i;
+        $media = number_format($media, 2, '.', '');
+    echo ("A Média dos números digitados é $media");
 }
 }
